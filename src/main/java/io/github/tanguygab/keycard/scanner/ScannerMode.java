fea611f3b485101accce7700edb2200e8827836d
Tanguygab <tanguy@gabril.com>
2022-01-30 15:11:32 +0100

    unused imports

--- src/main/java/io/github/tanguygab/keycard/scanner/ScannerMode.java
+++ src/main/java/io/github/tanguygab/keycard/scanner/ScannerMode.java
@@ -3,7 +3,6 @@ package io.github.tanguygab.keycard.scanner;
 import io.github.tanguygab.keycard.KeyCardPlugin;
 import org.bukkit.Material;
 
-import java.util.ArrayList;
 import java.util.List;
 import java.util.function.Consumer;
 
